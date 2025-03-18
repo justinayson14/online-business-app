e764d27e9a48ddfc6f96c129bc00380339a90880
Justin Ayson <121000453+justinayson14@users.noreply.github.com>
2025-03-17 23:30:29 -0700

    Adding boilerplate code & connecting app to mongodb

---
+++ src/main/java/_5/group2/online_business_system/OnlineBusinessSystemApplication.java
@@ -0,0 +1,13 @@
+package _5.group2.online_business_system;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnlineBusinessSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OnlineBusinessSystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/_5/group2/online_business_system/OnlineBusinessSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package _5.group2.online_business_system;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnlineBusinessSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
