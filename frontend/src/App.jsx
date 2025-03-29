a71e72f691e925c86160c24e043a0228664616eb
SeebassB <sebastian@igloomusic.com>
2025-03-28 18:05:57 -0700

    Toolbars now detect and change between user personas.

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,15 +1,25 @@
 import "./App.css";
+import { useState} from "react";
 import ProductCard from "./components/ProductCard";
 import LoginToolbar from "./components/LoginToolBar";
 import AdminToolbar from "./components/AdminToolBar";
 import UserToolbar from "./components/UserToolBar"
 
 function App() {
+
+  const [userRole, setUserRole] = useState("guest"); // admin, user, or guest)
+
   return (
     <>
-      <LoginToolbar />
-      <AdminToolbar />
-      <UserToolbar />
+      
+    {userRole === "guest" && (
+      <LoginToolbar 
+      onLoginUser={() => setUserRole("user")}
+      onLoginAdmin={() => setUserRole("admin")}
+      />
+    )}
+      {userRole === "admin" && <AdminToolbar onLogout={() => setUserRole("guest")} />}
+      {userRole === "user" && <UserToolbar onLogout={() => setUserRole("guest")} />}
       <ProductCard product={{name: "Keyboard", cost:"$20"}}/>
     </>
   );
