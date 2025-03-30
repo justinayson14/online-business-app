cdb7250846d3febb0187b11635633d0dd35a029b
Justin Ayson <121000453+justinayson14@users.noreply.github.com>
2025-03-29 20:45:52 -0700

    Added CrossOrigin to fix CORS errro

--- backend/src/main/java/_5/group2/online_business_system/OnlineBusinessSystemApplication.java
+++ backend/src/main/java/_5/group2/online_business_system/OnlineBusinessSystemApplication.java
@@ -2,6 +2,7 @@ package _5.group2.online_business_system;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
 @SpringBootApplication
 public class OnlineBusinessSystemApplication {
