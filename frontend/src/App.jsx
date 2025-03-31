8b048d97260e0bf1fa7204ee71c18ea32d56dc8f
Justin Ayson <121000453+justinayson14@users.noreply.github.com>
2025-03-31 03:13:44 -0700

    Removed unused functions

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -19,63 +19,11 @@ const darkTheme = createTheme({
 });
 
 function App() {
-  const [userRole, setUserRole] = useState("guest"); // admin, user, or guest)
-  const [showLoginModal, setShowLoginModal] = useState(false);
-  const [loginType, setLoginType] = useState(""); // user or admin
-
-  const handleLoginClick = (type) => {
-    console.log("Login button clicked:", type); // Debugging
-    setLoginType(type);
-    setShowLoginModal(true);
-  };
-
-  // Authenticate user (dummy check for now)
-  const handleLoginSubmit = (username, password) => {
-    console.log("Logging in:", username, password); // Debugging
-
-    if (
-      loginType === "user" &&
-      username === "user" &&
-      password === "password"
-    ) {
-      setUserRole("user");
-    } else if (
-      loginType === "admin" &&
-      username === "admin" &&
-      password === "adminpass"
-    ) {
-      setUserRole("admin");
-    } else {
-      alert("Invalid credentials!");
-    }
-    setShowLoginModal(false);
-  };
-
   return (
     <ThemeProvider theme={darkTheme}>
       <LoginProvider>
         <CssBaseline />
         <main className="main-content">
-          {/* {userRole === "guest" && (
-            <LoginToolbar
-              onLoginUser={() => handleLoginClick("user")}
-              onLoginAdmin={() => handleLoginClick("admin")}
-            />
-          )}
-          {userRole === "admin" && (
-            <AdminToolbar onLogout={() => setUserRole("guest")} />
-          )}
-          {userRole === "user" && (
-            <UserToolbar onLogout={() => setUserRole("guest")} />
-          )} */}
-          {/* Show login modal if needed */}
-          {/* {showLoginModal && (
-            <LoginModal
-              type={loginType}
-              onClose={() => setShowLoginModal(false)}
-              onSubmit={handleLoginSubmit}
-            />
-          )} */}
           <Routes>
             <Route path="/" element={<Home />} />
             <Route path="/list" element={<WishList />} />
